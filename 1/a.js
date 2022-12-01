50f5ae624e890053b5fad2f96d8c0623b837325b
Alexander Lind <alex.anonym@gmail.com>
2022-12-01 11:23:06 +0100

    finished day 1

--- 1/a.js
+++ 1/a.js
@@ -15,11 +15,24 @@ fs.readFile("./input.txt", "utf8", (err, data) => {
     .map((item) => item.split(" "));
   //   console.log(elfCaloriesArr);
   //   const elfCalorieCount = elfCaloriesArr.map((item) => {console.log(item)});
-  console.log(
-    elfCaloriesArr.forEach((item) =>
-      item.map((str) => {
-        return Number(str);
-      })
-    )
+  const elfCalorieCount = elfCaloriesArr.map((item) =>
+    item.map((str) => {
+      return Number(str);
+    })
   );
+  const topThreeElfs = []
+  const elfCalorieCountSum = elfCalorieCount.map((item) => item.reduce((acc, curr) => acc + curr));
+  const max1 = Math.max(...elfCalorieCountSum)
+  topThreeElfs.push(max1);
+  index1 = elfCalorieCountSum.indexOf(max1);
+  elfCalorieCountSum.splice(index1,1)
+  const max2 = Math.max(...elfCalorieCountSum)
+  topThreeElfs.push(max2);
+  index2 = elfCalorieCountSum.indexOf(max2);
+  elfCalorieCountSum.splice(index2,2)
+  const max3 = Math.max(...elfCalorieCountSum)
+  topThreeElfs.push(max3);
+  index3 = elfCalorieCountSum.indexOf(max3);
+  elfCalorieCountSum.splice(index3,3)
+  console.log(topThreeElfs.reduce((acc, curr) => acc + curr));
 });
