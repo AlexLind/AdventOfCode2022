ffd9279de68da89251703cfebf98604689a5750a
Alexander Lind <alex.anonym@gmail.com>
2022-12-02 08:49:46 +0100

    finished day 2

---
+++ 2/a.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+
+fs.readFile("./input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  strategyGuide = data.split(/\r?\n/).join(" ").split(" ");
+
+  const action = (action, reaction) => {
+    switch (reaction) {
+      case "Y":
+        if (action === "A") {
+          score += win + paper;
+        } else if (action === "B") {
+          score += draw + paper;
+        } else {
+          score += lose + paper;
+        }
+        break;
+      case "X":
+        if (action === "A") {
+          score += draw + rock;
+        } else if (action === "B") {
+          score += lose + rock;
+        } else {
+          score += win + rock;
+        }
+        break;
+      case "Z":
+        if (action === "A") {
+          score += lose + scissors;
+        } else if (action === "B") {
+          score += win + scissors;
+        } else {
+          score += draw + scissors;
+        }
+        break;
+    }
+  };
+
+  /*
+Y = Paper
+X = Rock
+Z = Scissors
+A = Rock
+B = Paper
+C = Scissors
+*/
+
+  const win = 6;
+  const draw = 3;
+  const lose = 0;
+  const rock = 1;
+  const paper = 2;
+  const scissors = 3;
+
+  let score = 0;
+
+  for (let i = 0; i < strategyGuide.length; i++) {
+    if (i % 2 === 1) {
+      switch (strategyGuide[i - 1]) {
+        case "A":
+          action("A", strategyGuide[i]);
+          break;
+        case "B":
+          action("B", strategyGuide[i]);
+          break;
+        case "C":
+          action("C", strategyGuide[i]);
+          break;
+      }
+    }
+  }
+
+  console.log(score);
+});
